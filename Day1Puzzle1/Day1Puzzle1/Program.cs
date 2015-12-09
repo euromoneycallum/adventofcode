80a0dfc66391c31d9b2b84d415459f459c05d2c4
Callum Steele <callum.steele@EULONWL09254.global.root>
2015-12-09 12:57:35 +0000

    Implemented second part of puzzle.

--- Day1Puzzle1/Day1Puzzle1/Program.cs
+++ Day1Puzzle1/Day1Puzzle1/Program.cs
@@ -15,6 +15,24 @@ namespace Day1Puzzle1
 
 			Console.WriteLine($"You've ended up on floor {resultingFloor}.");
 			Console.ReadKey();
+
+			var currentPosition = 1;
+			var currentFloor = 0;
+			foreach (var ch in testFileText)
+			{
+				if (ch == '(')
+					currentFloor++;
+				if (ch == ')')
+					currentFloor--;
+
+				if (currentFloor == -1)
+					break;
+
+				currentPosition++;
+			}
+
+			Console.WriteLine($"The position that resulted in first reaching the -1 floor was {currentPosition}");
+			Console.ReadKey();
 		}
 	}
 }
