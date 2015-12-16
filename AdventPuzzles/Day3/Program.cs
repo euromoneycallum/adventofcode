2d356115990a84eebe1bc4ef1bcc8016dd5b9454
Callum Steele <callum.steele4@hotmail.com>
2015-12-16 19:56:28 +0000

    Completed problem three part two, and refactored code.

--- AdventPuzzles/Day3/Program.cs
+++ AdventPuzzles/Day3/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using NUnit.Framework;
 
 namespace Day3
@@ -10,45 +11,86 @@ namespace Day3
 		public static void Main(string[] args)
 		{
 			var directions = File.ReadAllText("testfile.txt");
-			var timesAtLocation = LocationCalc.TimesAtLocation(directions);
+			var timesAtLocation = LocationCalc.LocationsVisited(directions);
 
 			Console.WriteLine($"{timesAtLocation.Count} houses received at least one present.");
 			Console.ReadKey();
-		}
+
+		    var locationsVisited = LocationCalc.LocationsVisitedRobo(directions);
+
+            Console.WriteLine($"{locationsVisited.Count} houses received at least one present.");
+            Console.ReadKey();
+        }
 	}
+
+    public class Pointer
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
 
 	public static class LocationCalc
 	{
-		public static Dictionary<Tuple<int, int>, int> TimesAtLocation(string directions)
-		{
-			int x = 0, y = 0;
-			var timesAtLocation = new Dictionary<Tuple<int, int>, int>();
-			timesAtLocation.Add(new Tuple<int, int>(x, y), 1);
+		public static Dictionary<Tuple<int, int>, int> LocationsVisited(string directions)
+        {
+            var santa = new Pointer { X = 0, Y = 0 };
+            var locationsVisited = new Dictionary<Tuple<int, int>, int>
+            {
+                {
+                    new Tuple<int, int>(0, 0), 1
+                }
+            }; ;
 
-			foreach (var direction in directions)
-			{
-				switch (direction)
-				{
-					case '^':
-						y++;
-						break;
-					case '>':
-						x++;
-						break;
-					case 'v':
-						y--;
-						break;
-					case '<':
-						x--;
-						break;
-				}
-				if (timesAtLocation.ContainsKey(new Tuple<int, int>(x, y)))
-					timesAtLocation[new Tuple<int, int>(x, y)]++;
-				else
-					timesAtLocation.Add(new Tuple<int, int>(x, y), 1);
-			}
-			return timesAtLocation;
+		    return directions
+                .Aggregate(locationsVisited, 
+                    (current, direction) => UpdateLocationsVisited(direction, santa, current));
 		}
+
+        public static Dictionary<Tuple<int, int>, int> LocationsVisitedRobo(string directions)
+        {
+            var santa = new Pointer { X = 0, Y = 0 };
+            var roboSanta = new Pointer { X = 0, Y = 0 };
+            var isDirectionForSanta = true;
+            var locationsVisited = new Dictionary<Tuple<int, int>, int>
+            {
+                {
+                    new Tuple<int, int>(0, 0), 1
+                }
+            };
+
+            foreach (var direction in directions)
+            {
+                locationsVisited = UpdateLocationsVisited(direction, isDirectionForSanta ? santa : roboSanta, locationsVisited);
+                isDirectionForSanta = !isDirectionForSanta;
+            }
+
+            return locationsVisited;
+        }
+
+	    private static Dictionary<Tuple<int, int>, int> UpdateLocationsVisited(char direction, Pointer pointer, Dictionary<Tuple<int, int>, int> locationsVisited)
+	    {
+	        switch (direction)
+	        {
+	            case '^':
+	                pointer.Y++;
+	                break;
+	            case '>':
+                    pointer.X++;
+	                break;
+	            case 'v':
+                    pointer.Y--;
+	                break;
+	            case '<':
+                    pointer.X--;
+	                break;
+	        }
+	        if (locationsVisited.ContainsKey(new Tuple<int, int>(pointer.X, pointer.Y)))
+	            locationsVisited[new Tuple<int, int>(pointer.X, pointer.Y)]++;
+	        else
+	            locationsVisited.Add(new Tuple<int, int>(pointer.X, pointer.Y), 1);
+
+            return locationsVisited;
+	    }
 	}
 
 	[TestFixture]
@@ -57,11 +99,21 @@ namespace Day3
 		[TestCase(">", 2)]
 		[TestCase("^>v<", 4)]
 		[TestCase("^v^v^v^v^v", 2)]
-		public void TimesAtLocation_GivenDirections_ReturnsCorrectHousesVisited(string directions, int expectedVisitedHouses)
+		public void LocationsVisited_GivenDirections_ReturnsCorrectHousesVisited(string directions, int expectedVisitedHouses)
 		{
-			var timesAtLocation = LocationCalc.TimesAtLocation(directions);
+			var timesAtLocation = LocationCalc.LocationsVisited(directions);
 
 			Assert.That(timesAtLocation.Count, Is.EqualTo(expectedVisitedHouses));
-		}
-	}
+        }
+
+        [TestCase("^v", 3)]
+        [TestCase("^>v<", 3)]
+        [TestCase("^v^v^v^v^v", 11)]
+        public void LocationsVisitedRobo_GivenDirections_ReturnsCorrectHousesVisited(string directions, int expectedVisitedHouses)
+        {
+            var timesAtLocation = LocationCalc.LocationsVisitedRobo(directions);
+
+            Assert.That(timesAtLocation.Count, Is.EqualTo(expectedVisitedHouses));
+        }
+    }
 }
