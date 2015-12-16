69969d790c6408f2680fbcedce65fa0472cf39fd
Callum Steele <callum.steele4@hotmail.com>
2015-12-16 21:48:34 +0000

    Implemented day 5 part 1 puzzle.

---
+++ AdventPuzzles/Day5/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+
+namespace Day5
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var words = File.ReadLines("testfile.txt");
+
+            var numberOfNiceWords = words.Count(NaughtyOrNiceCalc.IsNice);
+
+            Console.WriteLine($"The number of nice words is: {numberOfNiceWords}");
+            Console.ReadKey();
+        }
+    }
+
+    public static class NaughtyOrNiceCalc
+    {
+        private static readonly HashSet<char> Vowels = new HashSet<char> {'a', 'e', 'i', 'o', 'u'};
+        private static readonly string[] BadStrings = {"ab", "cd", "pq", "xy"};
+
+        public static bool IsNice(string word)
+        {
+            return VowelCounter(word) >= 3 && DoubleLetterInRow(word) && !ContainsBadStrings(word);
+        }
+
+        private static int VowelCounter(string word)
+        {
+            return word.Count(character => Vowels.Contains(character));
+        }
+
+        private static bool DoubleLetterInRow(string word)
+        {
+            var prevCharacter = word[0];
+            foreach (var character in word.Skip(1))
+            {
+                if (prevCharacter == character)
+                    return true;
+                prevCharacter = character;
+            }
+            return false;
+        }
+
+        private static bool ContainsBadStrings(string word)
+        {
+            return BadStrings.Any(word.Contains);
+        }
+    }
+
+    [TestFixture]
+    public class NaughtyOrNiceCalcTests
+    {
+        [TestCase("ugknbfddgicrmopn", true)]
+        [TestCase("aaa", true)]
+        [TestCase("jchzalrnumimnmhp", false)]
+        [TestCase("haegwjzuvuyypxyu", false)]
+        [TestCase("dvszwmarrgswjxmb", false)]
+        public void IsNice_GivenString_ReturnsTrueIfNice(string word, bool expectedIsNice)
+        {
+            var isNice = NaughtyOrNiceCalc.IsNice(word);
+
+            Assert.That(isNice, Is.EqualTo(expectedIsNice));
+        }
+    }
+}
